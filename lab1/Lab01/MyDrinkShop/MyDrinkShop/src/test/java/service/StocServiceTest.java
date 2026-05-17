971faec197137b586eb7c0d7d6206e81697be174
GalatzanT <tudor1galatan@gmail.com>
2026-05-17 22:59:47 +0300

    added for intT testing

--- lab1/Lab01/MyDrinkShop/MyDrinkShop/src/test/java/service/StocServiceTest.java
+++ lab1/Lab01/MyDrinkShop/MyDrinkShop/src/test/java/service/StocServiceTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StocServiceTest {
-
+    ///WBT
     @Test
     public void TC01_ValidOrder() {
     assertTrue(true);
@@ -13,6 +13,19 @@ public class StocServiceTest {
 
     @Test
     void TC05_oneIngredient_sufficientStock_returnsTrue() {
+        assertTrue(true);
+    }
+    //intT
+    @Test
+    void TC1_add_product_stub() {
+        assertTrue(true);
+    }
+    @Test
+    void TC2_add_product_repo() {
+        assertTrue(true);
+    }
+    @Test
+    void TC3_add_product_FullIntegration() {
         assertTrue(true);
     }
 }
