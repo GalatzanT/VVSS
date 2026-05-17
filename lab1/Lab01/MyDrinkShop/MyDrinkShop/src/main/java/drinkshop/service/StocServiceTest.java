6d67ccfe676b68921c0dfa8e67f4d57805ad205f
GalatzanT <tudor1galatan@gmail.com>
2026-05-17 22:07:03 +0300

    created test cases for WBT

---
+++ lab1/Lab01/MyDrinkShop/MyDrinkShop/src/main/java/drinkshop/service/StocServiceTest.java
@@ -0,0 +1,18 @@
+package mydrinkshop.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StocServiceTest {
+
+    @Test
+    void TC01_reteta_null_returnsFalse() {
+        assertTrue(true);
+    }
+
+    @Test
+    void TC05_oneIngredient_sufficientStock_returnsTrue() {
+        assertTrue(true);
+    }
+}
